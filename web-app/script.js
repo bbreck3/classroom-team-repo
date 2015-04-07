5c208353790f8c104c230a13a32dc1c98ab3a841
bbreck3 <bbreck3@gmail.com>
2015-04-06 22:04:37 -0400

    updated all pages and fixed login issue

--- web-app/script.js
+++ web-app/script.js
@@ -3,3 +3,36 @@ document.getElementById(id).style.color=black;
 
 document.getElementById(id).style.backgroundColor="yellow";
 }
+
+function messages_redirect(){
+	window.location='messages.html'
+	
+	}
+
+function grades_redirect(){
+	window.location='grades.html'
+	
+	}
+
+function attendance_redirect(){
+	window.location='attendance.html'
+	
+	}
+
+function calendar_redirect(){
+	window.location='calendar.html'
+	
+	}
+function index_redirect(){
+	window.location='index.html'
+	
+	}
+function load_dash(){
+	window.location='index.html'
+	
+	}
+function redirect()
+    {
+    var url = "index.html";
+    window.location(url);
+    }
